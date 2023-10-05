1117b7d692e5b0e5b20e87d7fdd4cc9ea29f4691
@hexlet/cli <support@hexlet.io>
2023-10-05 23:14:11 +0700

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -1,6 +1,8 @@
 package exercise;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Array;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -13,13 +15,20 @@ class AppTest {
     @Test
     void testTake() {
         // BEGIN
-        List<Integer> coll = new ArrayList<>();
-        Assertions.assertEquals(App.take(coll, 0).size(), 0);
-        coll.add(1);
-        coll.add(2);
-        coll.add(3);
-        Assertions.assertEquals(App.take(coll, 10).size(), 3);
-        Assertions.assertEquals(App.take(coll, 1).size(), 1);
+        List<Integer> coll1 = new ArrayList<>();
+        List<Integer> expected1 = new ArrayList<>();
+        List<Integer> result1 = App.take(coll1, 15);
+        Assertions.assertEquals(result1, expected1);
+
+        List<Integer> coll2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> expected2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> result2 = App.take(coll2, 28);
+        Assertions.assertEquals(result2, expected2);
+
+        List<Integer> coll3 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> expected3 = new ArrayList<>(Arrays.asList(1, 2));
+        List<Integer> result3 = App.take(coll3, 2);
+        Assertions.assertEquals(result3, expected3);
         // END
     }
 }
